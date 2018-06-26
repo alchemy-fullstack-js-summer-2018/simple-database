d488874993cceb1ce31fa94883521d2693f96724
MariahAdams <mariah.adams23@yahoo.com>
2018-06-26 16:47:49 -0700

    added store files to lib and test folders

---
+++ test/store.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const {  } = require('../lib/store');
+
+describe('store', () => {
+
+    describe('save', () => {
+
+        it('', () => {
+            
+        });
+    });
+});
