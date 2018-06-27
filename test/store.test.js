e50e59fcc01deba9ea0219cb1c4279d08305c07d
Mario Quintana <pyrros2000@gmail.com>
2018-06-27 11:12:19 -0700

    Store test is written out, check linter and begin store save function

--- test/store.test.js
+++ test/store.test.js
@@ -1,26 +1,30 @@
+const Store = require('../lib/store');
 const assert = require('assert');
 const path = require('path');
-const { prosimify } = require('util')
-const { rimraf, mkdirp,  readdir } = require('../lib/fs');
+const { rimraf, mkdirp } = require('../lib/fs');
 
-// const Store = require('../lib/store');
-// const rootDirectory = path.join(__dirname, 'animals');
-// const store = new Store(rootDirectory);
 
 describe('store', () => {
 
     const dir = path.join(__dirname, 'animals');
+    let store = new Store(dir);
 
     beforeEach(() => {
-        return rimraf (dir);
+        return rimraf(dir);
     });
 
     beforeEach(() => {
-        return mkdirp (dir);
+        return mkdirp(dir);
     });
 
     it('save', () => {
-        const store = new Store(dir);
-    });
 
-})
+        return store.save({ name: 'garfield' })
+            .then(animal => {
+                store.get(animal._id);
+            })
+            .then(animal =>{
+                assert.ok('got animal', animal._id)
+            })
+    });
+});
