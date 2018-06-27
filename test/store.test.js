f82ba3436f779403d34942055a3c3beec00d6815
Ryan Luras <ryanluras@gmail.com>
2018-06-26 17:19:17 -0700

    simple passing test

--- test/store.test.js
+++ test/store.test.js
@@ -1,2 +1,14 @@
 const assert = require('assert');
+const { travisTest } = require('../lib/store');
 
+describe('store project', () =>
+
+    describe('travisTest', () => {
+        it('should pass a test to get travis going', () => {
+            const numbers = [1, 2, 3, 4];
+            const result = travisTest(numbers, (n) => n * n);
+            assert.deepEqual(result, [1, 4, 9, 16]);
+        });
+    })
+
+);
