6b123f3467a90876e848d04339f155f6e4b7ff79
Rehmsy <sarahrehmer@gmail.com>
2018-06-27 11:18:22 -0700

    added some code to test, not sure if correct.

--- test/store.test.js
+++ test/store.test.js
@@ -1,26 +1,27 @@
 const assert = require('assert');
 const Store = require('../lib/store');
+const path = require ('path');
+const fs = require('fs');
+const { rimraf, readdir, writeFile } = require('../lib/store');
 
 
-describe('store', () => {
-    let newStore;
-    beforeEach(() => {
-        newStore = new Store();
+describe('store some pet data', () => {
+    const dest = path.join(__dirname, 'pets');
+    const store = new Store(dest);
+
+    beforeEach(()  => {
+        return rimraf(dest);
     });
+    beforeEach(()  => {
+        return mkdirp(dest);
+    }); 
+    
+    it('save a pet with id and return', () => {
 
-    const pets = [
-        {
-            name: 'Benjamin Frankilin',
-            type: 'cat'
-        },
-        {
-            name: 'Birdtrude Stein',
-            type: 'bird'
-        }, 
-        {
-            name: 'Milo',
-            type: 'cat'
-        }
-    ];
- 
-});
+        store.save({ name: 'Benjamin Franklin' });
+    }
+});  
+    
+    
+
+
