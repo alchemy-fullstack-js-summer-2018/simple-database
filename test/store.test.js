27ba51a7614c5a3c8d787260ff48b113866f4481
Ryan Luras <ryanluras@gmail.com>
2018-06-27 12:16:31 -0700

    getting a file by id

--- test/store.test.js
+++ test/store.test.js
@@ -6,11 +6,14 @@ const { rimraf, mkdirp } = require('../lib/fs');
 
 describe('Store Database Project', () => {
 
+    //describe the destination of our database by joining the path and the database name:
     const dest = path.join(__dirname, 'database');
+    //create a new Store/database with the attached root directory
     let store = new Store(dest);
 
     beforeEach(() => {
         return rimraf(dest);
+
     });
 
     beforeEach(() => {
@@ -18,10 +21,23 @@ describe('Store Database Project', () => {
     });
 
     it('saves a file to the Database with an id', () => {
-        return store.save({ file: 'file data contents' })
+        //use our .save() create a store file with these callback parameters:
+        return store.save({ file: 'file contents' })
+        //now check to see if this file matches the parameters we are looking for:
             .then(saved => {
-                assert.equal(saved.file, 'file data contents');
+                assert.equal(saved.file, 'file contents');
             });
     });
+
+
+    it('gets a file based on its id', () =>  {
+        return store.save({ file: 'file data contents' })
+        //use our .get() to verify the file id:
+            .then(verify => {
+                return store.get(verify._id);
+            });
+    });
+
+
    
 });
