1b2ee65524f5e1a101ab6791c3a678ffebfdd5a2
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-27 12:31:48 -0700

    Returns null if no animal id is found test successful

--- test/store.test.js
+++ test/store.test.js
@@ -1,26 +1,13 @@
 const assert = require('assert');
 const path = require('path');
 const { promisify } = require('util');
-const { readFile } = require('../lib/fs');
 const rimraf = promisify(require('rimraf'));
 const mkdirp = promisify(require('mkdirp'));
 const Store = require('../lib/store');
 
-describe('Sample Test', () => {
-    
-    it('Returns each array item + 1', () => {
-        const store = new Store();
-        const arr = [1, 2, 3];
-        const result = store.plusOne(arr, (item) => item + 1);
-        assert.deepEqual(result, [2, 3, 4]);
-    });
-    
-});
-
 
 describe('Store some animal data', () => {
 
-    let store = null;
     const dest = path.join(__dirname, 'animals');
     
     beforeEach(() => {
@@ -55,7 +42,10 @@ describe('Store some animal data', () => {
 
     it('Returns null if no animal id is found', () => {
         const store = new Store(dest);
-        return store.getFile('fakeId')
+        store.saveFile({ name: 'Sparky' })
+            .then(animal => {
+                return store.getFile(JSON.parse(animal)._id);
+            })
             .then(animal => {
                 assert.equal(animal.id, null);
             });
