716375d6e8f9f9f6837fd72b4cb7adb0a348a45d
MariahAdams <mariah.adams23@yahoo.com>
2018-06-27 09:51:58 -0700

    added animals dir to lib, added setup for tests, starting save test

--- test/store.test.js
+++ test/store.test.js
@@ -1,12 +1,33 @@
 const assert = require('assert');
-const {  } = require('../lib/store');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const rimraf = promisify(require('rimraf'));
+const mkdirp = promisify(require('mkdirp'));
+const shortid = require('shortid');
+const { save } = require('../lib/store');
+const Store = require('../lib/store');
+
+
 
 describe('store', () => {
 
-    describe('save', () => {
+    const dir = path.join(__dirname, 'animals');
 
-        it('', () => {
-            
-        });
+    beforeEach(() => {
+        return rimraf(dir);
+    });
+
+    beforeEach(() => {
+        return mkdirp(dir);
+    });
+
+    it('save an object with an id', () => {
+        const store = new Store(dir);
+
+        return store.save({ name: 'garfield' })
+            .then(animal => {
+                assert.ok(animal._id);
+            });
     });
 });
