2db6f10cb5c10a7dd19593e7a3761c8fc14cb52f
Ryan Luras <ryanluras@gmail.com>
2018-06-27 13:17:47 -0700

    removing comments

--- test/store.test.js
+++ test/store.test.js
@@ -6,9 +6,7 @@ const { rimraf, mkdirp } = require('../lib/fs');
 
 describe('Store Database Project', () => {
 
-    //describe the destination of our database by joining the path and the database name:
     const dest = path.join(__dirname, 'database');
-    //create a new Store/database with the attached root directory
     let store = new Store(dest);
 
     beforeEach(() => {
@@ -21,18 +19,14 @@ describe('Store Database Project', () => {
     });
 
     it('saves a file to the Database with an id', () => {
-        //use our .save() create a store file with these callback parameters:
         return store.save({ file: 'file contents' })
-        //now check to see if this file matches the parameters we are looking for:
             .then(saved => {
                 assert.equal(saved.file, 'file contents');
             });
     });
 
-
     it('gets a file based on its id', () =>  {
         return store.save({ file: 'file contents' })
-        //use our .get() to verify the file id:
             .then(verify => {
                 return store.get(verify._id);
             });
