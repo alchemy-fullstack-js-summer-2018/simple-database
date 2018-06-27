66f43bd2ba646409b8d21ff60395a92a04a1b1c3
Mark Myers <markalope@gmail.com>
2018-06-27 08:58:58 -0700

    working on save file test code

--- test/database.test.js
+++ test/database.test.js
@@ -1,1 +1,23 @@
-const variableName = require('variableName');
+const assert = require('assert');
+const shortid = require('shortid');
+const { unlink } = require('../lib/fs');
+
+const id = shortid.generate();
+console.log(id);
+
+describe('save file', () => {
+
+    const saveDir = path.join(__dirname, 'animals');
+    const destFileName = id + '.json';
+    const dest = path.join(saveDir, destFileName);
+
+    beforeEach(() => {
+        return unlink(dest)
+            .catch(err => {
+                if(err.code !== 'ENOENT') throw err;
+            });
+    });
+    it('saves file to database directory', () => {
+        return
+    })
+});
