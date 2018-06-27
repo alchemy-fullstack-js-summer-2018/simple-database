6376f5d4484db5b1eaa20dbe54dd92817ba733b6
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-27 10:57:13 -0700

    Initial commit for store.test.js

---
+++ test/store.test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const { rimraf, mkdirp } = require('fs');
+const path = require('path');
+
+const Store = require('../lib/store.js');
+
+describe('Store some animal data', () => {
+    const dest = path.join(__dirname, 'animals');
+    const store = new Store(dest);
+
+    beforeEach(() => {
+        return rimraf(dest);
+    });
+
+    beforeEach(() => {
+        return mkdirp(dest);
+    });
+
+    it('Save a file to animals directory with an id', () => {
+        store.save({ name: 'garfield' })
+            .then(animal => {
+                return store.get(animal._id);
+            })
+            .then(animal => {
+                console.log('got animal & id', animal + animal._id);
+            })
+            .catch(err => console.log(err));
+    });
+
+});
+
