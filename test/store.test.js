e804380e967b41531a344e21bf164c9ac8f4a34a
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-27 10:23:58 -0700

    Save file test set up

--- test/store.test.js
+++ test/store.test.js
@@ -1,12 +1,41 @@
 const assert = require('assert');
-const { plusOne } = require('../lib/store');
+const path = require('path');
+const { promisify } = require('util');
+const rimraf = promisify(require('rimraf'));
+const mkdirp = promisify(require('mkdirp'));
+const Store = require('../lib/store');
 
-describe('Sample test', () => {
-
-    it('tests item + 1', () => {
+describe('Sample Test', () => {
+    
+    it('Returns each array item + 1', () => {
+        const store = new Store();
         const arr = [1, 2, 3];
-        const result = plusOne(arr, (item) => item + 1);
+        const result = store.plusOne(arr, (item) => item + 1);
         assert.deepEqual(result, [2, 3, 4]);
+    });
+    
+});
+
+
+describe('Store some animal data', () => {
+
+    const dest = path.join(__dirname, 'animals');
+    
+    beforeEach(() => {
+        return rimraf(dest);
+    });
+    
+    beforeEach(() => {
+        return mkdirp(dest);
+    });
+    
+    it('Saves a file to animals directory with an id', () => {
+        const store = new Store(dest);
+
+        return store.saveFile({ name: 'Frank' })
+            .then(animal => {
+                assert.ok(JSON.parse(animal)._id);
+            });
     });
 
 });
