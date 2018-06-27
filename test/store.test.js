f34b6d502c47f974c1d6858b6f6decda9c7ac7b6
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-27 11:44:49 -0700

    Gets animal based on id test and function passing

--- test/store.test.js
+++ test/store.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const path = require('path');
 const { promisify } = require('util');
+const { readFile } = require('../lib/fs');
 const rimraf = promisify(require('rimraf'));
 const mkdirp = promisify(require('mkdirp'));
 const Store = require('../lib/store');
@@ -19,6 +20,7 @@ describe('Sample Test', () => {
 
 describe('Store some animal data', () => {
 
+    let store = null;
     const dest = path.join(__dirname, 'animals');
     
     beforeEach(() => {
@@ -36,6 +38,19 @@ describe('Store some animal data', () => {
             .then(animal => {
                 assert.ok(JSON.parse(animal)._id);
             });
+    });
+
+    it('Gets an animal based on the id', () => {
+        const store = new Store(dest);
+
+        store.saveFile({ name: 'Doggo' })
+            .then(animal => {
+                return store.getFile(JSON.parse(animal)._id);
+            })
+            .then(animal => {
+                assert.equal(animal.name, 'Doggo');
+            })
+            .catch(err => console.log(err));
     });
 
 });
