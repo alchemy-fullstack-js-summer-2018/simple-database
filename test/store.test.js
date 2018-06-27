6ceb7e64630fcf875a78a9c061e4f68aadfbea7d
Rehmsy <sarahrehmer@gmail.com>
2018-06-27 12:01:35 -0700

    omg, real test finally failing

--- test/store.test.js
+++ test/store.test.js
@@ -1,26 +1,39 @@
 const assert = require('assert');
-const Store = require('../lib/store');
-const path = require ('path');
 const fs = require('fs');
-const { rimraf, readdir, writeFile } = require('../lib/store');
-
+const path = require ('path');
+const { promisify } = require('util');
+const rimraf = promisify(require('rimraf'));
+const mkdirp = promisify(require('mkdirp'));
+const shortid = require('shortid');
+const { save } = require('../lib/store');
+const Store = require('../lib/store');
 
 describe('store some pet data', () => {
-    const dest = path.join(__dirname, 'pets');
-    const store = new Store(dest);
+    const dir = path.join(__dirname, 'pets');
 
     beforeEach(()  => {
-        return rimraf(dest);
+        return rimraf(dir);
     });
+
     beforeEach(()  => {
-        return mkdirp(dest);
+        return mkdirp(dir);
     }); 
     
     it('save a pet with id and return', () => {
-
-        store.save({ name: 'Benjamin Franklin' });
-    }
-});  
+        const store = new Store(dir);
+        
+        return store.save({ name: 'Benjamin Franklin' })
+            .then(pet => {
+                assert.ok(pet._id);
+                return store.get(pet._id);
+                //(shortid.generate) ??
+            })
+            .then(pet => {
+                assert.equal(pet.name, 'Benjamin Franklin');
+                console.log('got pet', pet);
+            }); 
+    });  
+}); 
     
     
 
