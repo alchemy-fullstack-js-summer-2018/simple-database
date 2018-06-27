192744a64870823c5933321355b6c34900187cff
rbtprograms <rbt61690@gmail.com>
2018-06-27 10:31:35 -0700

    returns null when given bad id

--- test/store.test.js
+++ test/store.test.js
@@ -5,20 +5,23 @@ const path = require('path');
 describe.only('this is my store function', () => {
 
     const source = path.join(__dirname, 'save-file-dir/');
+    const item = new Store(source);
 
-    it('saves obj', () => {
-        const item = new Store(source);
-        item.save({name: 'james'})
+    it('saves obj and gets obj successfully', () => {
+        item.save({name: 'DOGS'})
             .then(obj => {
                 console.log(obj._id)
                 return item.get(obj._id);
             })
             .then(animal => {
-                assert.equal(animal.name, 'james');
+                assert.equal(animal.name, 'DOGS');
             });
     });
 
-    // it('removes files', () => {
-        
-    // })
+    it('returns null when given a bad id', () => {
+        return item.get('HHH')
+            .then(obj => {
+                assert.equal(obj, null);
+            });      
+    })
 });
