1143c792e36fbc6f0c9f134c201e429b8c7e5494
rbtprograms <rbt61690@gmail.com>
2018-06-27 09:05:17 -0700

    saving is wokring but in wrong directory

---
+++ test/store.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const Store = require('../lib/store');
+const path = require('path');
+
+describe.only('this is my store function', () => {
+
+    const source = path.join(__dirname, 'save-file-dir');
+
+    it('saves shit', () => {
+        // console.log(source);
+        const item = new Store(source);
+        item.save({name: 'Bobby'})
+            .then(saved => {
+                assert.ok(JSON.parse(saved)._id)
+            });
+    });
+});
