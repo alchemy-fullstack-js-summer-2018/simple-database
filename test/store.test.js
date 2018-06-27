a5f6aa9ba6f5973197139106beb697d608140524
Ryan Luras <ryanluras@gmail.com>
2018-06-27 13:53:08 -0700

    fixing some stuff, hopefully

--- test/store.test.js
+++ test/store.test.js
@@ -21,21 +21,15 @@ describe('Store Database Project', () => {
     it('saves a file to the Database with an id', () => {
         return store.save({ file: 'file contents' })
             .then(saved => {
-                assert.equal(saved.file, 'file contents');
-            });
-    });
-
-    it('gets a file based on its id', () =>  {
-        return store.save({ file: 'file contents' })
-            .then(verify => {
-                return store.get(verify._id);
+                assert.ok(saved._id);
+                return store.get(saved._id);
             });
     });
 
     it('looks for file and returns null if it does not exist', () => {
         return store.get('fake-file')
-            .then(verify => {
-                assert.equal(verify, null);
+            .then(result => {
+                assert.equal(result, null);
             });
     });
 
