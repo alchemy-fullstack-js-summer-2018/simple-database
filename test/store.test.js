41af75e2aaf066572d04aa7ed23c7c2279dc1cd3
Ryan Luras <ryanluras@gmail.com>
2018-06-27 10:41:54 -0700

    stupid linter

--- test/store.test.js
+++ test/store.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const path = require('path');
-const Store = require('../lib/Store');
+const Store = require('../lib/store');
 const { rimraf, mkdirp } = require('../lib/fs');
 
 
