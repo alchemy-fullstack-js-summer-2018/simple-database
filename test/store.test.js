c10d29985beab086994f11f2925f23ff6506449b
Robyn Navarro <rnavarro182@gmail.com>
2018-06-27 21:41:42 -0700

    getAll method tests passed

--- test/store.test.js
+++ test/store.test.js
@@ -4,6 +4,19 @@ const { rimraf, mkdirp } = require('../lib/fs');
 const path = require('path');
 const rootDir = path.join(__dirname, 'warehouse');
 
+//from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function sortGods(a, b) {
+    const godA = a._id.toUpperCase();     
+    const godB = b._id.toUpperCase(); 
+    if(godA < godB) {
+        return -1;
+    }
+    if(godA > godB) {
+        return 1;
+    }
+    return 0;
+}
+
 describe('store', () => {
 
     let myStore = null;
@@ -35,7 +48,7 @@ describe('store', () => {
     });
         
     it('removes file with specified id', () => {
-        return myStore.save({ god: 'Zeus' })
+        return myStore.save({ god: 'Kratos' })
             .then(obj => {
                 return myStore.remove(obj._id);
             })
@@ -43,6 +56,29 @@ describe('store', () => {
                 assert.equal(status.removed, true);       
             });   
     });
-    
+
+    it('gets all files', () => {
+        const gods = [
+            { god: 'Zeus' },
+            { god: 'Kratos' },
+            { god: 'FSM' }
+        ];
+        return Promise.all(gods.map(god => {
+            return myStore.save(god);
+        }))
+            .then(() => {
+                return myStore.getAll();
+            })
+            .then(objList => {
+                assert.deepEqual(objList.sort(sortGods), gods.sort(sortGods));
+            });
+    });
+
+    it('returns an empty array', () => {
+        return myStore.getAll()
+            .then(objList => {
+                assert.deepEqual(objList, []);
+            });
+    });
 
 });
